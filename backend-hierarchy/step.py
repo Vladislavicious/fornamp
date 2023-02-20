9fadad56cc50b9e5309949a6af6edd2f1101efa1
vladislavicious <vladshilov03@mail.ru>
2023-02-20 20:21:16 +0300

    Добавил сериализацию/десериализацию в JSON, возможно в будущем переработаю её

--- backend-hierarchy/step.py
+++ backend-hierarchy/step.py
@@ -1,8 +1,14 @@
+import json
+
+class simpleEncoder(json.JSONEncoder):
+    """Нужен для нормального перевода класса в JSON"""
+    def default(self, o):
+        return o.__dict__
 
 class Step:
-    def __init__(self, name, ) -> None:
-        self.__isDone = False
-        self.__contributor = "No-one"
+    def __init__(self, name: str, isDone=False, contributor="No-one") -> None:
+        self.__isDone = isDone
+        self.__contributor = contributor
 
         self.__name = name.lower()
     
@@ -11,10 +17,21 @@ class Step:
             return f"Шаг {self.__name}. Исполнитель: {self.__contributor}"
         return f"Шаг {self.__name} не выполнен"
 
-    def MarkAsDone(self, contributor) -> None:
+    def MarkAsDone(self, contributor: str) -> None:
         self.__isDone = True
-        self.__contributor = contributor.capitalize()
+        self.__contributor = contributor.capitalize() 
     
-boba = Step("Подводка")
-boba.MarkAsDone("Жека")
-print(boba)
+    def toJSON(self):
+        return json.dumps(self, cls=simpleEncoder, sort_keys=True, indent=4, ensure_ascii=False)
+
+    @classmethod
+    def fromJSON(cls, jsonString):
+        """Возвращает объект класса Step из строки(формата JSON)"""
+        info = json.loads(jsonString)
+        
+        return Step(info["_Step__name"], info["_Step__isDone"], info["_Step__contributor"])
+
+    
+boba = Step("Подкраска")
+
+print(Step.fromJSON(boba.toJSON()))
