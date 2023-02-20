1fa82bbd71384a1efee3d88a066b267496d35b9c
vladislavicious <vladshilov03@mail.ru>
2023-02-20 21:58:16 +0300

    Добавил товар

---
+++ backend-hierarchy/product.py
@@ -0,0 +1,58 @@
+import json
+from step import Step
+from step import simpleEncoder
+
+class Product:
+    def __init__(self, name : str, selling_cost : int, \
+                    production_cost = 0, commentary = "", \
+                    steps = list(), isDone = False) -> None:
+
+        self.__name = name.capitalize()
+        self.__selling_cost = selling_cost
+        self.__production_cost = production_cost
+        self.__commentary = commentary
+        self.__steps = steps
+        self.__isDone = isDone
+    
+    def GetName(self) -> str:
+        return self.__name
+
+    def GetSteps(self):
+        return self.__steps
+
+    def GetCommentary(self) -> str:
+        return self.__commentary
+
+    def ChangeCommentary(self, new_commentary: str):
+        self.__commentary = new_commentary
+    
+    def addStep(self, step_name):
+        self.__steps.append(Step(step_name))
+    
+    def CheckIfDone(self) -> bool:
+        for step in self.__steps:
+            if step.CheckIfDone() == False:
+                return False
+        self.__isDone = True
+        return True
+
+    def GetProfit(self) -> int:
+        return self.__selling_cost - self.__production_cost
+
+    def __str__(self) -> str:
+        """вывод инф-и о классе для отладки"""
+        step_str = "\n".join(list(step.__str__() for step in self.__steps))
+        return f"{self.__name} стоит {self.__selling_cost}. Для выполнения нужно выполнить следующие шаги: \n{step_str}"
+
+    def toJSON(self):
+        return json.dumps(self, cls=simpleEncoder, sort_keys=True, indent=4, ensure_ascii=False)
+
+    @classmethod
+    def fromJSON(cls, jsonString):
+        """Возвращает объект класса Product из строки(формата JSON)"""
+        info = json.loads(jsonString)
+        
+        step_list = list(Step.fromDict(step) for step in info["_Product__steps"])
+
+        return Product(info["_Product__name"], info["_Product__selling_cost"], info["_Product__production_cost"], \
+                            info["_Product__commentary"], step_list, info["_Product__isDone"])            
--- backend-hierarchy/step.py
+++ backend-hierarchy/step.py
@@ -18,20 +18,25 @@ class Step:
         return f"Шаг {self.__name} не выполнен"
 
     def MarkAsDone(self, contributor: str) -> None:
-        self.__isDone = True
+        """Меняет готовность на противоположную"""
+        self.__isDone = not (self.__isDone)
         self.__contributor = contributor.capitalize() 
     
+    def CheckIfDone(self) -> bool:
+        return self.__isDone
+
     def toJSON(self):
         return json.dumps(self, cls=simpleEncoder, sort_keys=True, indent=4, ensure_ascii=False)
 
     @classmethod
-    def fromJSON(cls, jsonString):
+    def fromJSON(cls, jsonString: str):
         """Возвращает объект класса Step из строки(формата JSON)"""
         info = json.loads(jsonString)
         
         return Step(info["_Step__name"], info["_Step__isDone"], info["_Step__contributor"])
 
-    
-boba = Step("Подкраска")
-
-print(Step.fromJSON(boba.toJSON()))
+    @classmethod
+    def fromDict(cls, info : dict):
+        """Возвращает объект класса Step из словаря"""
+        
+        return Step(info["_Step__name"], info["_Step__isDone"], info["_Step__contributor"])
