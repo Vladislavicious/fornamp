d7ef9ed423e92e3ae12abcdfffaa6cc7f66824d1
vladislavicious <vladshilov03@mail.ru>
2023-02-27 11:35:13 +0300

    добавил коэффициенты в Шаг

--- backend-hierarchy/step.py
+++ backend-hierarchy/step.py
@@ -6,10 +6,13 @@ class simpleEncoder(json.JSONEncoder):
         return o.__dict__
 
 class Step:
-    def __init__(self, name: str, isDone=False, contributor="No-one") -> None:
+    def __init__(self, name: str, isDone=False, contributor="No-one", \
+                    complexity = 1, koef_value = 1) -> None:
         self.isDone = isDone
         self.contributor = contributor
         self.name = name
+        self.complexity = complexity
+        self.koef_value = koef_value
     
     @property
     def isDone(self):
@@ -35,14 +38,39 @@ class Step:
     def name(self, name: str):
         self.__name = name.lower()
 
+    @property
+    def complexity(self):
+        return self.__complexity
+    
+    @complexity.setter
+    def complexity(self, complexity: int):
+        """Сложность шага, 1 - лёгкий, 2 - средний, 3 - тяжёлый"""
+        self.__complexity = complexity
+
+        if complexity < 1:
+            self.__complexity = 1 # Easy
+        if complexity > 3:
+            self.__complexity = 3 # Hard
+
+    @property
+    def koef_value(self):
+        return self.__koef_value
+    
+    @koef_value.setter
+    def koef_value(self, koef_value: float):
+        """Цена шага отосительно товара"""
+        self.__koef_value = koef_value
+
+        if koef_value < 0 or koef_value > 1: 
+            raise ValueError # Для отладки
+
     def __str__(self) -> str:
         if self.__isDone:
             return f"Шаг {self.name}. Исполнитель: {self.contributor}"
         return f"Шаг {self.name} не выполнен"
 
-    def MarkAsDone(self, contributor: str) -> None:
-        """Меняет готовность на противоположную"""
-        self.isDone = not (self.isDone)
+    def MarkAsDone(self, contributor: str, done = True) -> None:
+        self.isDone = done
         self.contributor = contributor 
 
     def toJSON(self):
@@ -53,15 +81,20 @@ class Step:
         """Возвращает объект класса Step из строки(формата JSON)"""
         info = json.loads(jsonString)
         
-        return Step(info["_Step__name"], info["_Step__isDone"], info["_Step__contributor"])
+        return Step.fromDict(info)
 
     @classmethod
     def fromDict(cls, info : dict):
         """Возвращает объект класса Step из словаря"""
         
-        return Step(info["_Step__name"], info["_Step__isDone"], info["_Step__contributor"])
+        return Step(info["_Step__name"], info["_Step__isDone"], info["_Step__contributor"], \
+                        info["_Step__complexity"], info["_Step__koef_value"])
 
 
 aboba = Step("Шаг")
+aboba.isDone = True
+aboba.complexity = 2
 
-print(aboba.toJSON())
+print(lol := Step.fromJSON(aboba.toJSON()))
+
+print(lol.toJSON())
