2750df8214cc7c0fc2c196de10cfc66563f5ce63
Vladislavicious <107302903+Vladislavicious@users.noreply.github.com>
2023-03-26 14:20:53 +0300

    Sorting (#9)
    * Добавил пример отчёта
    
    * Изменил имена на подходящие
    
    * Добавил простенькую визуализацию
    
    * Добавил функцию сравнения двух работников на графике

---
+++ Caps/Visualising.py
@@ -0,0 +1,50 @@
+"""функции для отображения информации"""
+import pandas as pd
+pd.plotting.register_matplotlib_converters()
+import matplotlib.pyplot as plt
+import seaborn as sns
+from typing import List
+
+from Caps.listFuncs import *
+from Caps.sorting import *
+import BaH
+
+def SaveBarplot(data : pd.Series, startDate : datetime.datetime, endDate : datetime.datetime, title: str = ""):
+    """Сохраняет график в рабочей директории"""
+    series = data[startDate:endDate]
+
+    label_rotation = 45
+    if(series.size > 75):
+        label_rotation = 90
+
+    fig, ax = plt.subplots(figsize = (16,8))    
+    fig = sns.barplot(x = series.index, y = series.values, ax=ax)
+
+    x_labels = series.index.strftime('%d-%m-%Y')
+
+    ax.set_title(title)
+    ax.set_ylabel("Проделанная работа, раб. коэф")
+    ax.set_xticklabels(labels=x_labels, rotation=label_rotation, ha='right')
+
+    save_fig = fig.get_figure()
+    save_fig.savefig(title+".png")
+
+def SaveLinechartComparison(startDate : datetime.datetime, endDate : datetime.datetime, series_list : List[pd.Series], title: str = ""):
+    series = list()
+    for ser in series_list:
+        series.append(ser[startDate : endDate])
+    
+    x_labels = pd.date_range(startDate, endDate, freq="D")
+
+    df = pd.DataFrame(index=x_labels)
+    for ser in series:
+        df[ser.name] = ser
+
+    fig, ax = plt.subplots(figsize = (16,8))    
+    fig = sns.lineplot(data=df, ax=ax)
+
+    ax.set_title(title)
+    ax.set_ylabel("Проделанная работа, раб. коэф")
+
+    save_fig = fig.get_figure()
+    save_fig.savefig(title+".png")
