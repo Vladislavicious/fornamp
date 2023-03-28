3877e540277224b2667e8612d2f931d43b605ba4
vladislavicious <vladshilov03@mail.ru>
2023-03-28 22:57:57 +0300

    Убрал часть глобальных переменных

--- main.py
+++ main.py
@@ -5,12 +5,4 @@ from GUI.GUI import *
 
 
 if __name__ == "__main__":
-    root = ctk.CTk()
-    font_ = ctk.CTkFont(family="Arial", size=16)
-    fontmini = ctk.CTkFont(family="Arial", size=12)
-    app = Main_window(root)
-    app.pack()
-    root.title("Task manager")
-    root.geometry("1000x600+250+100")
-    root.resizable(False, False)
-    root.mainloop()
+    MainLoop()
