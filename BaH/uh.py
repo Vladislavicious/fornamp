8c0884c1a164f39eebb048fe4b4667a5a0712304
vladislavicious <vladshilov03@mail.ru>
2023-05-12 12:29:58 +0300

    Добавил возможность авторизации

--- BaH/uh.py
+++ BaH/uh.py
@@ -7,7 +7,7 @@ class UserHandler:
     """
     Считывает пользователей из папки, хранит в себе пользователей, может добавлять, убирать.
     В том числе содержит функции для проверки Логина и имейла
-    После любого изменения пользователей, требуется вызвать SaveToFile
+    После любого изменения пользователей, сам вызывает SaveToFile
     """
     def __init__(self, key: bytes, filepath: str = "", users: list = list()) -> None:
         self.users = users
@@ -19,9 +19,13 @@ class UserHandler:
         self.__seekLastUser()   # Если список юзеров пуст, то переменная self.lastUser равна None
 
     def SaveToFile(self, filepath: str = "Перезапись"):
+        """без проверки на существование файла"""
         path = filepath
         if filepath == "Перезапись":
             path = self.filepath
+        else:
+            self.filepath = filepath
+
         if len(self.users) == 0:
             return
         
@@ -61,6 +65,20 @@ class UserHandler:
 
         self.lastUser = lastUser
 
+    def Authorize(self, login: str, password: str) -> bool:
+        """Если пользователь авторизован, возвращает True и ставит его lastUser'ом"""
+        if not self.__lexicLoginValidation(login):
+            return False   # Нет смысла проходить по всем юзерам, если логин не может существовать в первую очередь
+
+        for user in self.users:
+            if login == user.login:
+                if password == user.password:
+
+                    self.lastUser = user
+                    return True
+                
+        return False
+    
     def markAsLastUser(self, login: str) -> bool:
         """Возвращает True, если пользователь найден и отмечен последним"""
         self.setNoLastUsers()
@@ -69,20 +87,26 @@ class UserHandler:
             if login == user.login:
                 user.isLastUser = True
                 self.lastUser = user
+
+                self.SaveToFile()
                 return True
 
         return False
 
     def setNoLastUsers(self):
+        """Сохраняет, только если что-то изменилось"""
+        isChanged = False
         for user in self.users:
-            user.isLastUser = False
+            if user.isLastUser is True:
+                user.isLastUser = False
+                isChanged = True
+
         self.lastUser = None
-
-    def ValidateLogin(self, login: str) -> bool:
-        """Проверяет можно ли создать аккаунт с таким логином"""
-        if len(self.users) == 0:
-            return True
-
+        if isChanged:
+            self.SaveToFile()
+    
+    def __lexicLoginValidation(self, login: str) -> bool:
+        """Проверяет символы в логине"""
         login_len = len(login)
 
         if login_len > 32 or login_len < 8:
@@ -91,6 +115,16 @@ class UserHandler:
         for i in login.lower():
             if i not in '1234567890qwertyuiopasdfghjklzxcvbnm':
                 return False
+            
+        return True
+
+    def ValidateLogin(self, login: str) -> bool:
+        """Проверяет можно ли создать аккаунт с таким логином"""
+        if not self.__lexicLoginValidation(login):
+            return False
+        
+        if len(self.users) == 0:
+            return True
 
         for user in self.users:
             if login == user.login:
@@ -98,7 +132,7 @@ class UserHandler:
         return True
     
     def ValidateEmail(self, email: str) -> bool:
-        """Проверка синтаксическая, без проверки на существование"""
+        """Проверка синтаксическая, без проверки на существование такого адреса"""
         pattern = r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$'
 
         if re.match(pattern, email):
